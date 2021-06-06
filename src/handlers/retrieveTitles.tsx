4bddfcdd7a219dfaa12521468d317aa67eb679c8
Kevin Hu <kevinhuwest@gmail.com>
2021-06-05 22:58:47 -0700

    Working chrome+firefox

--- src/handlers/retrieveTitles.tsx
+++ src/handlers/retrieveTitles.tsx
@@ -1,7 +1,7 @@
 import retrieveTitle from './retrieveTitle'
 
 const retrieveTitles = async (urls: Array<string>) => {
-    const titles = Promise.all(urls.map((url) => retrieveTitle(url)))
+    const titles = await Promise.all(urls.map((url) => retrieveTitle(url)))
     return titles
 }
 
